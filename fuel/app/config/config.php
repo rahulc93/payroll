f5837d4b29d71f557b18a4605fd533cab1b608bf
Rahul Chowdhuy <rahulc93@gmail.com>
2014-08-15 11:56:23 +0530

    Set default timezone to Asia/Kolkata

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -42,7 +42,7 @@ return array(
 	 *
 	 * Set this to 'index.php if you don't use URL rewriting
 	 */
-	// 'index_file' => false,
+	'index_file' => 'index.php',
 
 	// 'profiling'  => false,
 
@@ -94,7 +94,7 @@ return array(
 	 * default_timezone		optional, if you want to change the server's default timezone
 	 */
 	// 'server_gmt_offset'  => 0,
-	// 'default_timezone'   => null,
+	'default_timezone'   => 'Asia/Kolkata',
 
 	/**
 	 * Logging Threshold.  Can be set to any of the following:
